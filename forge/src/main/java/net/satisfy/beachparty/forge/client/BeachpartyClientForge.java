c3f64e352a8a6596ddcef326fed32adee6df3e71
satisfy <104036234+satisfyu@users.noreply.github.com>
2025-02-20 21:04:30 +0100

    fix(forgemixin): HelmetItemMixin, DyeableArmorMixin fixed

--- forge/src/main/java/net/satisfy/beachparty/forge/client/BeachpartyClientForge.java
+++ forge/src/main/java/net/satisfy/beachparty/forge/client/BeachpartyClientForge.java
@@ -1,12 +1,18 @@
 package net.satisfy.beachparty.forge.client;
 
+import net.minecraft.client.model.BoatModel;
+import net.minecraft.client.model.ChestBoatModel;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.registries.RegisterEvent;
 import net.satisfy.beachparty.Beachparty;
 import net.satisfy.beachparty.client.BeachPartyClient;
+import net.satisfy.beachparty.core.entity.PalmBoatEntity;
 
 @Mod.EventBusSubscriber(modid = Beachparty.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BeachpartyClientForge {
@@ -19,6 +25,14 @@ public class BeachpartyClientForge {
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
         BeachPartyClient.initClient();
+    }
+
+    @SubscribeEvent
+    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        for (PalmBoatEntity.Type type : PalmBoatEntity.Type.values()) {
+            event.registerLayerDefinition(new ModelLayerLocation(new ResourceLocation(Beachparty.MOD_ID, type.getModelLocation()), "main"), BoatModel::createBodyModel);
+            event.registerLayerDefinition(new ModelLayerLocation(new ResourceLocation(Beachparty.MOD_ID, type.getChestModelLocation()), "main"), ChestBoatModel::createBodyModel);
+        }
     }
 
 }
--- forge/src/main/java/net/satisfy/beachparty/forge/mixin/HelmetItemMixin.java
+++ forge/src/main/java/net/satisfy/beachparty/forge/mixin/HelmetItemMixin.java
@@ -8,10 +8,12 @@ import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import net.satisfy.beachparty.core.item.TrinketsArmorItem;
 import net.satisfy.beachparty.core.registry.ArmorRegistry;
+import net.satisfy.beachparty.core.registry.ObjectRegistry;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
@@ -24,26 +26,45 @@ import java.util.function.Consumer;
 public abstract class HelmetItemMixin extends ArmorItem {
     @Shadow
     @Final
-    private ResourceLocation hatTexture;
+    private ResourceLocation getTexture;
 
     @Override
     public void initializeClient(Consumer<IClientItemExtensions> consumer) {
-        consumer.accept(
-                new IClientItemExtensions() {
-                    @Override
-                    public @NotNull Model getGenericArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
-                        return ArmorRegistry.HelmetModel(itemStack.getItem(), original.getHead());
-                    }
+        consumer.accept(new IClientItemExtensions() {
+            @Override
+            public @NotNull Model getGenericArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
+                Item item = itemStack.getItem();
+                if (item == ObjectRegistry.BEACH_HAT.get()) {
+                    return ArmorRegistry.HelmetModel(item, original.getHead());
+                } else if (item == ObjectRegistry.SUNGLASSES.get()) {
+                    return ArmorRegistry.HelmetModel(item, original.getHead());
+                } else if (item == ObjectRegistry.RUBBER_RING_PINK.get()) {
+                    return ArmorRegistry.chestplateModel(item, original.body, original.leftArm, original.rightArm);
                 }
-        );
+                else if (item == ObjectRegistry.RUBBER_RING_STRIPPED.get()) {
+                    return ArmorRegistry.chestplateModel(item, original.body, original.leftArm, original.rightArm);
+                }
+                else if (item == ObjectRegistry.RUBBER_RING_BLUE.get()) {
+                    return ArmorRegistry.chestplateModel(item, original.body, original.leftArm, original.rightArm);
+                }
+                else if (item == ObjectRegistry.RUBBER_RING_PELICAN.get()) {
+                    return ArmorRegistry.chestplateModel(item, original.body, original.leftArm, original.rightArm);
+                }
+                else if (item == ObjectRegistry.RUBBER_RING_AXOLOTL.get()) {
+                    return ArmorRegistry.chestplateModel(item, original.body, original.leftArm, original.rightArm);
+                }
+                return ArmorRegistry.HelmetModel(item, original.getHead());
+            }
+        });
     }
 
     @Override
     public @Nullable String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
-        return hatTexture.toString();
+        return getTexture.toString();
     }
 
     private HelmetItemMixin(ArmorMaterial armorMaterial, Type armorType, Properties itemProperties) {
         super(armorMaterial, armorType, itemProperties);
     }
 }
+
